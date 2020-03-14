7e1fb90ed32c9e5c03ee03f6223f6d4d840542e2
pavel.dvorak <pavel.dvorak@konicaminolta.cz>
2020-03-14 18:57:24 +0100

    Solved

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -10,14 +10,14 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 256 # 128  # minibatch size
+BATCH_SIZE = 1024 # 128  # minibatch size
 GAMMA = 0.99  # discount factor
 TAU = 1e-3  # for soft update of target parameters
 LR_ACTOR = 1e-4  # learning rate of the actor
 LR_CRITIC = 1e-3  # learning rate of the critic
 WEIGHT_DECAY = 0  # L2 weight decay
-UPDATE_EVERY = 10
-LEARN_TIMES = 5
+UPDATE_EVERY = 10  # how often to train the agent
+LEARN_TIMES = 5  # how many times to train the agent in a row
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -113,7 +113,7 @@ class Agent():
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
-        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 0.2) #########################
+        torch.nn.utils.clip_grad_norm_(self.critic_local.parameters(), 1) #########################
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
